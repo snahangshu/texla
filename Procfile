75b897435ad555a8c22321d4ae5888acf1b6e651
snahangshu mallik <mallik00bis@gmail.com>
2025-11-01 16:43:08 +0530

    first commit 1

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn texla_service.wsgi:application --bind 0.0.0.0:$PORT
+#web: gunicorn texla_service.wsgi:application --bind 0.0.0.0:$PORT
+web: bash start.sh
--- start.sh
+++ start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+set -o errexit  # exit on error
+
+# Move inside project directory
 cd texla_service1
+
+# Run Django management commands
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
+
+echo "Applying migrations..."
 python manage.py migrate
+
+echo "Starting Gunicorn server..."
 gunicorn texla_service.wsgi:application --bind 0.0.0.0:$PORT
