75cb665f243244a3e0852c7a2abd51719d5beed3
snahangshu mallik <mallik00bis@gmail.com>
2025-11-01 13:51:12 +0530

    Fix Django settings module path for Railway

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python texla_service/manage.py collectstatic --noinput
 python texla_service/manage.py migrate
-gunicorn texla_service.texla_service.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn texla_service1.texla_service.wsgi:application --bind 0.0.0.0:$PORT
