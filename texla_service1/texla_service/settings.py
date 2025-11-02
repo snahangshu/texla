b78b8612743d503bf75457e24565f6bdb632a510
snahangshu mallik <mallik00bis@gmail.com>
2025-11-02 17:02:15 +0530

    settings.py update

--- texla_service1/texla_service/settings.py
+++ texla_service1/texla_service/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         "ENGINE": "djongo",
         "NAME": "texla_crm",
         "CLIENT": {
-            "host": os.getenv("MONGO_URI"),
+            "host": "mongodb+srv://mallikbishal93_db_user:texla@cluster0.piegpvl.mongodb.net/texla_crm?retryWrites=true&w=majority",
             "tls": True,
             "tlsCAFile": certifi.where(),
         },
