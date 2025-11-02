96375c2d4bda2c1dd19fec4c453fb8a2fc3a5194
snahangshu mallik <mallik00bis@gmail.com>
2025-11-02 17:44:23 +0530

    updated start.sh

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Applying migrations..."
-python manage.py migrate
+python manage.py migrate --fake-initial
 
 echo "Starting Gunicorn server..."
 gunicorn texla_service.wsgi:application --bind 0.0.0.0:$PORT
